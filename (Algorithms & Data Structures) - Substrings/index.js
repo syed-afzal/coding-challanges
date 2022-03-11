cc5f16c8884949a50a15c589a5b646f29ca8d78f
Syed Afzal <sah.afzal@gmail.com>
2022-03-11 10:55:54 +0500

    Substrings better solution

--- (Algorithms & Data Structures) - Substrings/index.js
+++ (Algorithms & Data Structures) - Substrings/index.js
@@ -1,13 +1,52 @@
-const subStrings = (str) => {
+const dictionary = {
+  'a': 1,
+  'b': 2,
+  'c': 2,
+  'd': 2,
+  'e': 2,
+  'f': 3,
+  'g': 3,
+  'h': 3,
+  'i': 4,
+  'j': 4,
+  'k': 4,
+  'l': 5,
+  'm': 5,
+  'n': 5,
+  'o': 6,
+  'p': 6,
+  'q': 6,
+  'r': 7,
+  's': 7,
+  't': 7,
+  'u': 8,
+  'v': 8,
+  'w': 8,
+  'x': 9,
+  'y': 9,
+  'z': 9,
+};
+
+const generateSubStrings = (str) => {
   const result = [];
-  for (let i = 1; i <= str.length; i++) {
-    let k = 0;
-    console.log(str.length - i)
-    for (let j = 0 ; j <= str.length - i; j++) {
-      result.push(str.substring(k, i+j));
-      k++;
+  for(let i=0;i<str.length;i++){
+    for(let j=0;j+i+1<=str.length;j++){
+      result.push(str.substring(j,j+i+1))
     }
   }
   return result;
 };
-console.log(subStrings('bgh'));
+
+const subStringsCount = (inputString) => {
+  const subStrings = generateSubStrings(inputString);
+  let count = 0;
+  subStrings.forEach((str) => {
+    const sum = str.split('').reduce((acc, curr) => acc + dictionary[curr], 0);
+    if (sum % str.length === 0) {
+      count += 1;
+    }
+  });
+  return count;
+};
+
+console.log(subStringsCount('abdulabashirkhanwaseemasifwaleedafzalhunainasadullahjabirkhanasdflkjhfg'))
