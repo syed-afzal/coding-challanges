16ba3d0e631eec344b5755a853d53615b2393b01
Syed Afzal <sah.afzal@gmail.com>
2022-05-22 00:11:58 +0500

    Parking Bill completion :rocket:

--- Parking-Bill/index.js
+++ Parking-Bill/index.js
@@ -3,6 +3,19 @@ const calculateParkingBill = (E, L) => {
   L = L.split(':');
   let hoursDiff = L[0] - E[0];
   let minutesDiff = L[1] - E[1];
+  return 2 + 3 + (hoursDiff >= 1 ? ((hoursDiff - 1) * 4) : 0) + (hoursDiff >= 1 && minutesDiff > 1 ? 4 : 0);
 }
 console.log(calculateParkingBill("10:00", "13:21"))
-console.log(calculateParkingBill("10:00", "10:01"))
+// console.log(calculateParkingBill("10:00", "10:01"))
+// console.log(calculateParkingBill("10:00", "10:45"))
+// console.log(calculateParkingBill("17:00", "18:48"))
+
+
+// function solution(N) {
+//   let K = 0;
+//   while((N % Math.pow(2,K+1)) == 0) {
+//     K++;
+//   }
+//   return K;
+//   // write your code in JavaScript (Node.js 8.9.4)
+// }
