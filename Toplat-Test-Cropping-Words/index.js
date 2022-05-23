edf92bcc67a9e90e51e4ba4c44b1d6b6f0c9f5d1
Syed Afzal <sah.afzal@gmail.com>
2022-05-23 12:30:31 +0500

    Toptal test 2  Cropping Words :fire:

---
+++ Toplat-Test-Cropping-Words/index.js
@@ -0,0 +1,16 @@
+const solution = (message, K) => {
+  const words = message.split(' ');
+  let result = '';
+  let currentMessage = '';
+  for(let i=0; i<words.length; i++) {
+    currentMessage += words[i] + ' ';
+    if(currentMessage.length > K) {
+      break;
+    } else {
+      result += words[i] + ' ';
+    }
+  }
+  return result.trim();
+};
+
+console.log(solution('Codility We test coders', 14));
