760487f0730021279bd54047ade58b98868f811f
Syed Afzal <sah.afzal@gmail.com>
2022-05-23 12:32:58 +0500

    Toptal test 2  Cropping Words Bug fixes :fire:

--- Toplat-Test-Cropping-Words/index.js
+++ Toplat-Test-Cropping-Words/index.js
@@ -4,14 +4,15 @@ const solution = (message, K) => {
   let currentMessage = '';
   for(let i=0; i<words.length; i++) {
     currentMessage += words[i] + ' ';
-    if(currentMessage.length > K) {
+    if(currentMessage.length > K+1) {
       break;
     } else {
       result += words[i] + ' ';
     }
   }
+  console.log(result.length);
   return result.trim();
 };
 
 console.log(solution('Codility We test coders', 14));
-console.log(solution('Codility We test coders', 14));
+console.log(solution('The quick brown fox jumps over the lazy dog', 39));
