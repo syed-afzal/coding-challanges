fe0f4c8b4ffb1258e534330911a6c1fb104d2336
Syed Afzal <sah.afzal@gmail.com>
2022-05-25 13:24:32 +0500

    joined function :sparkles:

--- Toptal/index.js
+++ Toptal/index.js
@@ -1,3 +1,15 @@
+const joined = (word, result) => {
+  let currentWordIdx = result.indexOf(word[0]);
+  if (currentWordIdx === -1){
+    result.push(word[0])
+  } else {
+    let nextWordIdx = result.indexOf(word[1]);
+    if (nextWordIdx !== -1) {
+      result = [word[0], ...result]
+    }
+  }
+}
+
 const solution = (arr) => {
   let result = [];
   let word= '';
@@ -9,8 +21,8 @@ const solution = (arr) => {
   return result.join('');
 }
 
-console.log(solution(["P>E","E>R","R>U"]));
-console.log(solution(["I>N","A>I","P>A","S>P"]));
+console.log(solution(["P>E","E>R","R>U"])); // PERU
+console.log(solution(["I>N","A>I","P>A","S>P"])); // SPAIN
 console.log(solution(["I>F", "W>I", "S>W", "F>T"]));
 console.log(solution(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"])) // PORTUGAL
 console.log(solution(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"]))
