2077ac67d50b766d6f4f72279facc5472f0e0aae
Syed Afzal <sah.afzal@gmail.com>
2022-06-01 00:40:26 +0500

    Getting done with index solution :zap:

---
+++ Toptal/indexFindApproach.js
@@ -0,0 +1,39 @@
+const solutionOptimize = (param) => {
+  let result = [];
+  for (let i=0; i<param.length; i++) {
+
+    let word = param[i].split('>');
+    const idxFirst = result.indexOf(word[0])
+    const idxSecond = result.indexOf(word[1])
+
+    if (idxFirst === -1){
+      if (idxSecond === -1) result.push(word[0])
+      else {
+        result.splice(idxSecond, 0, word[0])
+      }
+    } else {
+      if (idxFirst > -1 && idxSecond > -1) {
+        if (idxFirst> idxSecond){
+          result.splice(idxFirst, 1)
+          result.splice(idxSecond, 0, word[0])
+        } else {
+          result.splice(idxSecond, 1)
+          result.splice(idxFirst+1, 0, word[1])
+        }
+      }
+    }
+
+    if (idxSecond === -1) {
+      if (idxFirst > -1) {
+        result.splice(idxFirst+1, 0, word[1])
+      } else result.push(word[1])
+    }
+  }
+  return result.join('');
+}
+
+console.log(solutionOptimize(["P>E","E>R","R>U"])); // PERU
+console.log(solutionOptimize(["I>N","A>I","P>A","S>P"])); // SPAIN
+console.log(solutionOptimize(["I>F", "W>I", "S>W", "F>T"]));
+console.log(solutionOptimize(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U"])) // PORTUGAL
+console.log(solutionOptimize(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"]))
