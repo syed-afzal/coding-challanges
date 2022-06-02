36d0fdb761fdce1ab66411eb15a18142af6f9c73
Syed Afzal <sah.afzal@gmail.com>
2022-06-02 11:43:46 +0500

    Toptal test Second approoach using Includes

--- Toptal/index.js
+++ Toptal/index.js
@@ -1,29 +1,30 @@
-const joined = (word, result) => {
-  let currentWordIdx = result.indexOf(word[0]);
-  if (currentWordIdx === -1){
-    result.push(word[0])
-  } else {
-    let nextWordIdx = result.indexOf(word[1]);
-    if (nextWordIdx !== -1) {
-      result = [word[0], ...result]
+const solution = (arr) => {
+  arr = arr.map(word => word.replace('>', ''))
+
+  let output = '';
+
+  for (let i=0; i<arr.length; i++){
+    if (output === '') {
+      output = arr[i];
+    } else {
+      const firsteLetter = arr[i][0];
+      const secondLetter = arr[i][1];
+
+      if (output.includes(firsteLetter)){
+        output = output.replace(firsteLetter, arr[i])
+      } else if (output.includes(secondLetter)) {
+        output = output.replace(secondLetter, arr[i])
+      } else {
+        output += arr[i]
+      }
     }
   }
+  console.log(output);
 }
 
-const solution = (arr) => {
-  let result = [];
-  let word= '';
-  for (let i=0; i<arr.length; i++) {
-    word = arr[i].split('>');
-    joined(word, result);
-  }
-  result = [...result, word[1]]
-  return result.join('');
-}
-
-console.log(solution(["P>E","E>R","R>U"])); // PERU
-console.log(solution(["I>N","A>I","P>A","S>P"])); // SPAIN
-console.log(solution(["I>F", "W>I", "S>W", "F>T"]));
-console.log(solution(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"])) // PORTUGAL
-console.log(solution(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"]))
+// console.log(solution(["P>E","E>R","R>U"])); // PERU
+// console.log(solution(["I>N","A>I","P>A","S>P"])); // SPAIN
+// console.log(solution(["I>F", "W>I", "S>W", "F>T"]));
+// console.log(solution(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"])) // PORTUGAL
+// console.log(solution(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"]))
 
