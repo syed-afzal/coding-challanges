2c91324d3c76e028e941d58dc87fd93123795294
Syed Afzal <sah.afzal@gmail.com>
2022-06-03 14:25:47 +0500

    Output case :fire:

---
+++ HackerRank/PlusMinus/index.js
@@ -0,0 +1,13 @@
+function plusMinus(arr) {
+  let positive = 0;
+  let negative = 0;
+  const length = arr.length;
+  for(let i=0; i<length; i++) {
+    arr[i] > 0 ? positive++ : arr[i] < 0 ? negative++ : '';
+  }
+  console.log((positive/ length).toFixed(6))
+  console.log((negative/length).toFixed(6))
+  console.log(((length - (positive+negative))/length).toFixed(6))
+}
+
+plusMinus([-4,3,-9])
