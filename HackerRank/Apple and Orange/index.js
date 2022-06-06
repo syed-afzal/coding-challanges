022801e95844b2d18cbb1f8cb49a5572f02e16ca
Syed Afzal <sah.afzal@gmail.com>
2022-06-06 19:07:12 +0500

    Apple Orange Dir

---
+++ HackerRank/Apple and Orange/index.js
@@ -0,0 +1,20 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  // Write your code here
+  const length = apples.length > oranges.length ? apples.length : oranges.length;
+  let appleCount = 0;
+  let orangeCount = 0;
+  let val = 0;
+  for(let i=0; i<length; i++) {
+    val = 0;
+    if(apples[i]) {
+      val = a + apples[i];
+      appleCount = (val >= s && val <= t) ? (appleCount+1) : appleCount
+    }
+    if(oranges[i]) {
+      val = a + oranges[i];
+      orangeCount = (val >= s && val <= t) ? (orangeCount+1) : orangeCount
+    }
+  }
+  console.log(appleCount);
+  console.log(orangeCount);
+}
