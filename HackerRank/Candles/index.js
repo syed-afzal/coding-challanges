e10e78be467b28bad6f287700a70c07dfeae86d3
Syed Afzal <sah.afzal@gmail.com>
2022-06-09 19:02:42 +0500

    Candles Test (Remove Extra Lines) :fire:

--- HackerRank/Candles/index.js
+++ HackerRank/Candles/index.js
@@ -1,7 +1,6 @@
 const numberOfCandles = (candles) => {
   const max = Math.max(...candles);
-  let count = candles.filter(c => c === max);
-  return count;
+  return candles.filter(c => c === max).length;
 }
 
 console.log(numberOfCandles([1,3,2,3]));
