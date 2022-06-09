bdb406f30581fa59e24353b6b5e82673a8180fed
Syed Afzal <sah.afzal@gmail.com>
2022-06-09 19:11:21 +0500

    Candles Test (Test Case) :fire:

--- HackerRank/Candles/index.js
+++ HackerRank/Candles/index.js
@@ -5,3 +5,4 @@ const numberOfCandles = (candles) => {
 
 console.log(numberOfCandles([1,3,2,3]));
 console.log(numberOfCandles([4,4,3,1]));
+console.log(numberOfCandles([4,4,3,1,10,10,2,,3,5,6,7,10,10,10,3,4,5,7]));
