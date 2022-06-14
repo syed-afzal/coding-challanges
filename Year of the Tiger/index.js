61b429b1a933782b1146afcb5ed8cf01b2d3211b
Syed Afzal <sah.afzal@gmail.com>
2022-06-14 16:10:18 +0500

    Year of the Tiger with BigO(N^2) :zap:

--- Year of the Tiger/index.js
+++ Year of the Tiger/index.js
@@ -6,12 +6,24 @@ const swapLetters = (t) => {
   }
 }
 
+const countNumberOfCandidates = (T, cand) => {
+  let count = 0;
+  for (let t of T) {
+    if (Object.values(swapLetters(t)).includes(cand))
+      count++;
+  }
+  return count;
+}
+
 const Solution = (T) => {
   let output = 0
   for (let t of T) {
     for (let cand of Object.values(swapLetters(t))) {
+      let sum = countNumberOfCandidates(T, cand);
+      output = Math.max(output, sum);
     }
   }
+  return output;
 }
 
 console.log(Solution(["aab", "cab", "baa", "baa"]))
