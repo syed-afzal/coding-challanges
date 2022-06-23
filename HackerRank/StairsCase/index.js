5d4ace2640cc60d00a9cdafad82216e143b28224
Syed Afzal <sah.afzal@gmail.com>
2022-06-23 19:32:15 +0500

    Stairs Case Inclined Complete :fire:

--- HackerRank/StairsCase/index.js
+++ HackerRank/StairsCase/index.js
@@ -1,13 +1,15 @@
 const stairsCase = (n) => {
-  for (let i = 1; i <= n; i++) {
+  let height = n-1;
+  for (let i = 0; i < n; i++) {
     let stairs = "";
-    for (let j = n; j > 0; j--) {
-      if (j <= i) {
-        stairs += "#";
-      } else {
+    for (let j = 0; j < n; j++) {
+      if (j < height) {
         stairs += " ";
+      } else {
+        stairs += "#";
       }
     }
+    height -= 1;
     console.log(stairs);
   }
 }
