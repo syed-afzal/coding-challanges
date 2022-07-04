38065a6b4d40d6c0c1848bc827bf75a86ec7f79b
Syed Afzal <sah.afzal@gmail.com>
2022-07-04 19:27:37 +0500

    Step 1

--- HackerRank/Bigger is Greater/index.js
+++ HackerRank/Bigger is Greater/index.js
@@ -1,16 +1,14 @@
 const biggerIsGreater =(w) => {
   // Write your code here
-  let sorted = w.split('').sort();
-  let result = sorted.slice();
-  for (let i=w.length-1; i>0; i--) {
+  w = w.split('');
+  let i;
+  for (i = w.length-1; i >= 0; i--) {
     if(w[i].localeCompare(w[i-1]) > 0) {
-      [result[i], result[i-1]] = [result[i-1], result[i]]
-      if(result.join('').localeCompare(w) > 0) {
+      [w[i], w[i-1]] = [w[i-1], w[i]]
         break;
-        return
-      }
     }
   }
-  console.log(result);
+  let substring = w.slice(i, w.length).sort().join('');
+  console.log(substring)
 }
-biggerIsGreater('hefg');
+biggerIsGreater('abcd');
