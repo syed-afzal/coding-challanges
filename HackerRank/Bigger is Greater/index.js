7671efa4694f24f89b658ea06b6a18e252cffcbb
Syed Afzal <sah.afzal@gmail.com>
2022-07-04 19:49:03 +0500

    Bigger is Greater :zap:

--- HackerRank/Bigger is Greater/index.js
+++ HackerRank/Bigger is Greater/index.js
@@ -1,14 +1,21 @@
 const biggerIsGreater =(w) => {
+  let orignal = w;
   // Write your code here
+  let isFound = false;
   w = w.split('');
   let i;
-  for (i = w.length-1; i >= 0; i--) {
-    if(w[i].localeCompare(w[i-1]) > 0) {
-      [w[i], w[i-1]] = [w[i-1], w[i]]
+  let currentItemIndex = w.length -1;
+  while(!isFound && currentItemIndex >= 0) {
+    for (i = currentItemIndex -1; i >= 0; i--) {
+      if(w[currentItemIndex].localeCompare(w[i]) > 0) {
+        isFound = true;
+        [w[currentItemIndex], w[i]] = [w[i], w[currentItemIndex]]
         break;
+      }
     }
+    currentItemIndex = isFound ? currentItemIndex : currentItemIndex - 1;
   }
-  let substring = w.slice(i, w.length).sort().join('');
-  console.log(substring)
+  let substring = w.slice(i+1, w.length).sort().join('');
 }
 biggerIsGreater('abcd');
+biggerIsGreater('dkhc');
