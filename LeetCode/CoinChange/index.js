77f5036533a68a4f6916cbc3d2ce047ad449e00b
Syed Afzal <sah.afzal@gmail.com>
2022-07-09 19:55:40 +0500

    Other LeetCode test cases :rocket:

--- LeetCode/CoinChange/index.js
+++ LeetCode/CoinChange/index.js
@@ -14,7 +14,9 @@ var coinChange = function(coins, amount) {
       }
     }
   }
-  return array[array.length-1];
+  return array[array.length-1] === arbitaryVal ? -1 : array[array.length-1];
 };
 
 console.log(coinChange([1, 2, 5], 11));
+console.log(coinChange([2], 3));
+console.log(coinChange([1], 0));
