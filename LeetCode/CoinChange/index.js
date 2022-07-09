3ae135acb6524310d5e1489b0c0266fa2714a668
Syed Afzal <sah.afzal@gmail.com>
2022-07-09 19:47:28 +0500

    Basic Logic Implementation Done :fire:

---
+++ LeetCode/CoinChange/index.js
@@ -0,0 +1,22 @@
+var coinChange = function(coins, amount) {
+  if(amount < 1)
+    return 0;
+
+  const arbitaryVal = amount + 1;
+  let array = Array(arbitaryVal).fill(arbitaryVal);
+  array[0] = 0;
+
+  console.log(amount, coins.length);
+  for(let i=1; i<= amount; i++) {
+    for (let coin = 0; coin < coins.length; coin++) {
+      let cacheValue = i - coins[coin] ;
+      console.log(cacheValue, coins[coin]);
+      if(cacheValue >= 0) {
+        array[i] = Math.min(array[cacheValue]+1, array[i])
+      }
+    }
+  }
+  console.log(array);
+};
+
+coinChange([1, 2, 5], 11);
