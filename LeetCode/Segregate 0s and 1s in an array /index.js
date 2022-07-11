f7fc35f084731510dbfc0762093f31ec676a973c
Syed Afzal <sah.afzal@gmail.com>
2022-07-11 14:54:11 +0500

    Completion of Segregate if 0s and 1s in an array :rocket:

--- LeetCode/Segregate 0s and 1s in an array /index.js
+++ LeetCode/Segregate 0s and 1s in an array /index.js
@@ -1,1 +1,12 @@
 // https://www.geeksforgeeks.org/segregate-0s-and-1s-in-an-array-by-traversing-array-once/
+
+const solution = (arr) => {
+  const result = [];
+  while(arr.length > 0) {
+    arr[0] === 0 ? result.unshift(arr.shift()) : result.push(arr.shift());
+  }
+  return result;
+}
+
+console.log(solution([0, 1, 0, 1, 0, 0, 1, 1, 1, 0] ))
+console.log(solution( [0,1,1,1,1,0,1,1,0]  ))
