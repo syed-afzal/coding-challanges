6161d6f4d8ba938c49a1626fd39b98cebaa0dfd1
Syed Afzal <sah.afzal@gmail.com>
2022-07-15 14:16:41 +0500

    encoding of tree node values :zap:

---
+++ LeetCode/Anything/index.js
@@ -0,0 +1,33 @@
+const array = [
+  {
+    id:1,
+    edges:[
+      {
+        id:2,
+        edges:[
+          {
+            id:3,
+            edges:[
+              {
+                id:4,
+                edges:[]
+              }
+            ]
+          }
+        ]
+      }
+    ],
+  }
+]
+
+const encode = (array) => {
+  let result = [];
+ while(array[0].edges.length > 0) {
+   result.push(array[0].id);
+   array = array[0].edges;
+ }
+ result.push(array[0].id);
+ return result;
+}
+
+console.log(encode(array));
