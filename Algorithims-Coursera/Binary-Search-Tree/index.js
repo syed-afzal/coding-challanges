17bf2d428ad63632308082ac858ac45699bbd1c4
Syed Afzal <sah.afzal@gmail.com>
2022-07-22 13:03:08 +0500

    Binary Search Tree Implementation (Insertion) :zap:

---
+++ Algorithims-Coursera/Binary-Search-Tree/index.js
@@ -0,0 +1,33 @@
+let root = null;
+function Node(value) {
+    this.value = value;
+    this.left =  null;
+    this.right =  null;
+}
+
+const insert = (root, value) => {
+  if(!root) {
+    root = new Node(value);
+    return root;
+  }
+
+  if(value < root.value) {
+    root.left = insert(root.left,  value);
+  } else {
+    root.right = insert(root.right, value);
+  }
+
+  return root;
+}
+
+root = insert(root, 10);
+root = insert(root, 7);
+root = insert(root, 23);
+root = insert(root, 27);
+root = insert(root, 21);
+root = insert(root, 9);
+root = insert(root, 5);
+
+console.log(root);
+
+
