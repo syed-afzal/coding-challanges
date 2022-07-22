20ec10854d3825e20692586996b99869c3542698
Syed Afzal <sah.afzal@gmail.com>
2022-07-22 13:03:52 +0500

    Timezone Conversions :rocket

---
+++ HackerRank/TimeZone/index.js
@@ -0,0 +1,15 @@
+function timeConversion(s) {
+  // Write your code here
+  const [hours, minutes, second] = s.split(':')
+  if(second.includes('AM')){
+    if(hours < 12)
+      return `${hours}:${minutes}:${second.slice(0,2)}`
+    else
+      return `00:${minutes}:${second.slice(0,2)}`
+  } else {
+    return `${hours < 12 ? +hours+12 : +hours}:${minutes}:${second.slice(0,2)}`
+  }
+}
+
+console.log(timeConversion('12:45:54PM'));
+console.log(timeConversion('07:05:45PM'));
