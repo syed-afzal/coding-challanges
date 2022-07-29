f4c366c45249da7cad0166a22e192ce1ef1b92a2
Syed Afzal <sah.afzal@gmail.com>
2022-07-29 16:42:06 +0500

    Coder of Rivia

---
+++ HackerRank/Magic Square/index.js
@@ -0,0 +1,27 @@
+// const arr = [0, 2, 3, 4, 1, 1, 1, 3, 1];
+const arr = [5,3,4,1,5,8,6,4,2];
+// const arr = [4,9,2,3,5,7,8,1,5];
+// const arr = [4,8,2,4,5,7,6,1,6];
+const magicSquare = (arr) => {
+  let N = 3;
+
+  let rowSum = (i) => [0,1,2].reduce((acc,val) => acc+arr[i*3 + val],  0);
+  let columnSum = (i) => [0,1,2].reduce((acc,val) => acc+arr[val*3 + i],  0);
+
+  let maximumSum = 15;
+  let cost = 0
+
+  for (let i=0; i<N; i++) {
+    for (let j=0; j<N; j++) {
+      let val = maximumSum - Math.max(rowSum(i), columnSum(j));
+      if (val > 0) {
+        let a = arr[i*3 + j];
+        arr[i*3 + j] += val;
+        cost += Math.abs(a-arr[i*3 + j]);
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(magicSquare(arr));
