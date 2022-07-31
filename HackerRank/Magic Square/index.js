cb5bfa32708f0deac82b5809c0d163ff128c119f
Syed Afzal <sah.afzal@gmail.com>
2022-07-31 17:04:05 +0500

    Magic Square Hacker Rank

--- HackerRank/Magic Square/index.js
+++ HackerRank/Magic Square/index.js
@@ -43,3 +43,27 @@ const magicSquare = (arr) => {
 };
 
 console.log(magicSquare(arr3));
+const magicSquareMinimumCost = (arr) => {
+  const possibleMagicSquares = [[[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+    [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+    [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+    [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+    [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+    [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+    [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+    [[2, 7, 6], [9, 5, 1], [4, 3, 8]]];
+
+  let minimumCost = 1000;
+  possibleMagicSquares.forEach(magicSquare => {
+    let cost = 0;
+    for (let i=0; i<3; i++) {
+      for (let j=0; j<3; j++) {
+        cost += Math.abs(magicSquare[i][j] - arr[i][j]);
+      }
+    }
+    if (cost < minimumCost) {
+      minimumCost = cost;
+    }
+  });
+  return minimumCost;
+}
