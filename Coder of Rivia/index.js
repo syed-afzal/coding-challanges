5bca915c4f05d7794e746e738155e40c03d931bd
Syed Afzal <sah.afzal@gmail.com>
2022-08-01 12:00:26 +0500

    Fixes

--- Coder of Rivia/index.js
+++ Coder of Rivia/index.js
@@ -1,7 +1,7 @@
-// const arr = [0, 2, 3, 4, 1, 1, 1, 3, 1];
+const arr = [0, 2, 3, 4, 1, 1, 1, 3, 1];
 // const arr = [5,3,4,1,5,8,6,4,2];
 // const arr = [4,9,2,3,5,7,8,1,5];
-const arr = [4,8,2,4,5,7,6,1,6];
+// const arr = [4,8,2,4,5,7,6,1,6];
 const magicSquare = (arr) => {
   let N = 3;
 
