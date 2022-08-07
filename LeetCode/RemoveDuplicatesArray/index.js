83aff24c2a1ab9a0e596f3b0bffdf8e36f25937c
Syed Afzal <sah.afzal@gmail.com>
2022-08-07 16:22:22 +0500

    remove duplicates from an array new approach :zap:

--- LeetCode/RemoveDuplicatesArray/index.js
+++ LeetCode/RemoveDuplicatesArray/index.js
@@ -1,8 +1,22 @@
 const removeDuplicatesArray = (array) => {
   // console.log(array)
-  return array.filter(
-    (element,  idx) => array.indexOf(element) === idx
-  )
+  // return array.filter(
+  //   (element,  idx) => array.indexOf(element) === idx
+  // )
+
+  let j = 0;
+  for (let i=0; i < array.length - 1; i++) {
+    if (array[i+1] !== array[j])
+      array[j++] = array[i+1];
+  }
+  array[j++] = array[array.length -1];
+  return j;
 }
 
-console.log(removeDuplicatesArray([1,1,2]))
+console.log(removeDuplicatesArray([1,1,2,3,4,4,5]))
+
+// var obj = { first: "John", last: "Doe" };
+//
+// for(let [key,value] of Object.entries(obj)) {
+//   console.log(key,  value);
+// }
