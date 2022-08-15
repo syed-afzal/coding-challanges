c43d89da84a8d96d1988a6f6f167a16920b3882a
Syed Afzal <sah.afzal@gmail.com>
2022-08-15 16:41:40 +0500

    Roman to Integer Complete Solution :zap:

---
+++ LeetCode/Roman to Integer/index.js
@@ -0,0 +1,24 @@
+var romanToInt = function(s) {
+  const dictionary = {
+    'I':1,
+    'V':5,
+    'X':10,
+    'L':50,
+    'C':100,
+    'D':500,
+    'M':1000
+  };
+  let output = 0;
+  let idx = 0;
+  while(idx < s.length){
+    if(dictionary[s[idx]] < dictionary[s[idx+1]]) {
+      output += dictionary[s[idx+1]] - dictionary[s[idx]];
+      idx += 2;
+    } else {
+      output += dictionary[s[idx]];
+      idx++;
+    }
+  }
+};
+romanToInt('IV');
+romanToInt('LVIII');
