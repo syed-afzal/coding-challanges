d7db2cf6808ffe112b29ea4a6ba1e359b83c56ee
Syed Afzal <sah.afzal@gmail.com>
2022-08-29 16:20:53 +0500

    Clost to Zero Additional Case

--- HackerRank/Closte To Any Number/index.js
+++ HackerRank/Closte To Any Number/index.js
@@ -8,3 +8,5 @@ const closeToZero = (arr) => {
 };
 
 console.log(closeToZero([-1, 2, 3, 4, 5]));
+console.log(closeToZero([-4, -3, -2, -1, 1]));
+console.log(closeToZero([7,-10, 13, 8, 4, -7.2,-12,-3.7,3.5,-9.6, 6.5,-1.7, -6.2,7]));
