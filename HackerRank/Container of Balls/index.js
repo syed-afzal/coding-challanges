69d8c7e8ee52ed1dbe9a2d065fd793436b50c18a
Syed Afzal <sah.afzal@gmail.com>
2022-10-14 01:56:44 +0500

    Container of Balls Done :zap:

---
+++ HackerRank/Container of Balls/index.js
@@ -0,0 +1,19 @@
+const organizeContainers = (container) => {
+  const n = container.length;
+  const sum = (a,b) => a+b;
+  const rowSum = container.map(row => row.reduce(sum, 0));
+  const colSum = Array(n).fill(0);
+  for(let i=0; i<n; i++) {
+    for(let j=0; j<n; j++) {
+      colSum[i] += container[j][i];
+    }
+  }
+  rowSum.sort((a, b) => a - b);
+  colSum.sort((a, b) => a - b);
+  console.log(rowSum.join(''));
+  console.log(colSum.join(''));
+  return rowSum.join('') === colSum.join('') ? 'Possible' : 'Impossible';
+
+}
+organizeContainers([[1, 1], [1, 1]]);
+organizeContainers([[0, 2], [1, 1]]);
