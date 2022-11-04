e23653d012e2c28c814f93a6fbc0b95ee68ab7a8
Syed Afzal <sah.afzal@gmail.com>
2022-11-04 18:09:21 +0500

    Contiguous Array LeetCode 564/564 Passed :rocket:

--- LeetCode/Contiguous Array/index.js
+++ LeetCode/Contiguous Array/index.js
@@ -1,15 +1,15 @@
 const findMaxLength = (nums) => {
   let count = 0;
   let lastSameCountIndex = -1;
-  for (let i=0; i<nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     let currentOne = 0;
     let currentZero = 0;
-    for (let j=i; j>=0; j--) {
+    for (let j = i; j >= 0; j--) {
       if (nums[j]) currentOne++;
       else currentZero++;
 
-      if (currentZero === currentOne){
-        if((currentZero+currentOne) > count){
+      if (currentZero === currentOne) {
+        if ((currentZero + currentOne) > count) {
           count = currentZero + currentOne;
           lastSameCountIndex = i;
           break;
@@ -23,7 +23,34 @@ const findMaxLength = (nums) => {
 
   console.log(count);
 }
-findMaxLength([0,0,1,0,1,0,0]);
-findMaxLength([0,1,0]);
-findMaxLength([0,0,0,1,1,1,0]);
-findMaxLength([0,0,1,0,0,0,1,1])
+// findMaxLength([0, 0, 1, 0, 1, 0, 0]);
+// findMaxLength([0, 1, 0]);
+// findMaxLength([0, 0, 0, 1, 1, 1, 0]);
+// findMaxLength([0, 0, 1, 0, 0, 0, 1, 1]);
+
+const findMaxLengthDynamic = (nums) => {
+  let counts = {};
+  for (let i = 0; i < nums.length; i++) {
+    let currentOne = 0;
+    let currentZero = 0;
+    for (let j = i; j >= 0; j--) {
+      if (nums[j]) currentOne++;
+      else currentZero++;
+
+      if (currentZero === currentOne) {
+        if (counts[j - 1]) {
+          counts[i] = counts[j - 1] + currentZero + currentOne;
+          break;
+        }
+        counts[i] = currentZero + currentOne;
+      }
+    }
+  }
+  return Object.values(counts).reduce((acc, val) => {
+    return val > acc ? val : acc
+  }, 0);
+}
+findMaxLengthDynamic([0, 0, 1, 0, 1, 0, 0]);
+findMaxLengthDynamic([0, 1, 0]);
+findMaxLengthDynamic([0, 0, 0, 1, 1, 1, 0]);
+findMaxLengthDynamic([0, 0, 1, 0, 0, 0, 1, 1]);
