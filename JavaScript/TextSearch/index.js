882eddc3216171d903d9391d6cd04fa4abf802fa
Syed Afzal <sah.afzal@gmail.com>
2022-11-10 18:20:54 +0500

    Text Search Completion :zap:

--- JavaScript/TextSearch/index.js
+++ JavaScript/TextSearch/index.js
@@ -1,16 +1,16 @@
 const textSearch = (string, query) => {
-  if (string.trim() === '' || query.trim() === '') return string;
-  let newString = string;
-  for(let i=0; i<string.length;) {
-    const substr = string.substring(i,  query.length);
-    if (substr.toLowerCase() === query.toLowerCase()){
+  // if (string.trim() === '' || query.trim() === '') return string;
+  // let newString = string;
+  // for(let i=0; i<string.length;) {
+  //   const substr = string.substring(i,  query.length);
+  //   if (substr.toLowerCase() === query.toLowerCase()){
+  //
+  //   }
+  // }
 
-    }
-  }
-
-  // if (query.length < 1) return str;
-  // const regex = new RegExp(query, 'gi')
-  // return str.length > 0 ? str.replace(regex, `<b>${query}</b>`) : str;
+  if (query.length < 1) return str;
+  const regex = new RegExp(query, 'gi')
+  return str.length > 0 ? str.replace(regex, `<b>${query}</b>`) : str;
 }
 
 // console.log(textSearch('The Quick Brown Fox Jumps Ov/er The Lazy Dog, hence Fox', 'Fox'));
